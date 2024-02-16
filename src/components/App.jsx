18f65db15b9196b89793576533704e70de8feb21
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2024-02-16 22:04:15 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
-import fetchImages from 'services/api';
 import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+import fetchImages from 'services/api';
 
 import { Notify } from 'notiflix';
-import Modal from './Modal/Modal';
-import Button from './Button/Button';
 
 export const App = ({ query }) => {
   const [inputValue, setInputValue] = useState('');
