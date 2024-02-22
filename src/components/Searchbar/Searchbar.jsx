45e4c2a5ec3d534ade0bba9da72d2eb9099489fb
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2024-02-22 21:04:05 +0100

    added useRef

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 
-function Searchbar({ onInputValue, onSubmit }) {
+function Searchbar({ onSubmit }) {
+  const inputRef = useRef();
   const [query, setQuery] = useState('');
+
+  const handleFocus = () => {
+    inputRef.current.focus();
+  };
 
   const handleChange = e => {
     const newQuery = e.currentTarget.value.toLowerCase();
     setQuery(newQuery);
-    onInputValue(newQuery);
   };
 
   const handleSubmit = e => {
@@ -18,7 +22,6 @@ function Searchbar({ onInputValue, onSubmit }) {
 
     setQuery('');
   };
-
   return (
     <header>
       <form onSubmit={handleSubmit} className={css.searchForm}>
@@ -26,13 +29,17 @@ function Searchbar({ onInputValue, onSubmit }) {
           className={css.searchFormInput}
           type="text"
           name="query"
-          value={query}
+          ref={inputRef}
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
           onChange={handleChange}
         />
-        <button type="submit" className={css.searchFormButton}>
+        <button
+          type="submit"
+          className={css.searchFormButton}
+          onClick={handleFocus}
+        >
           Search
         </button>
       </form>
