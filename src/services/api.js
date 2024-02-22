a153c5f4a9f907cba2fa9b69c5c919a69a1d6d58
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2024-02-22 21:04:57 +0100

    use object for saving params

--- src/services/api.js
+++ src/services/api.js
@@ -5,10 +5,10 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '34960396-ee79eba7e6dca12e9fa7bf6c6';
 
 const fetchImages = async (query, page) => {
-  const response = await axios.get(
+  const { data } = await axios.get(
     `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
-  return response.data;
+  return data;
 };
 
 export default fetchImages;
